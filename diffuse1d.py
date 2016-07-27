3ce3afa2296077cf6c73ee059cc9093802bd15e9
Danny Goldstein <dgold@berkeley.edu>
2016-07-26 23:27:32 -0700

    add atmosphere class and mixer class

--- diffuse1d.py
+++ diffuse1d.py
@@ -1,23 +1,23 @@
-
-__whatami__ = 'Crank nicolson solver for 1D spherical diffusion equation.'
-__author__ = 'Danny Goldstein <dgold@berkeley.edu>'
-
+import abc
 import scipy
 import numpy as np
 from scipy.integrate import odeint
 import scipy.sparse as sparse
 import scipy.sparse.linalg
-import time
 
-def diffuse1d(phi, D, x, t):
+__whatami__ = 'Mixing for simple supernova atmospheres.'
+__author__ = 'Danny Goldstein <dgold@berkeley.edu>'
+
+
+def _diffuse1d(phi, D, x, t):
     """Let a 1D concentration `phi` defined over spatial grid `x` diffuse
     over temporal grid `t` subject to (potentially) spatially varying
-    diffusion coefficient `D` by integrating the 1D diffusion equation
+    diffusion coefficient `D` by integrating the 1D diffusionx equation
     using a Crank Nicolson solver. Return the final concentration.
-    
+
     Parameters
     ---------- 
-    
+
     phi : array
         Concentration profile.
     D : array or scalar
@@ -31,16 +31,16 @@ def diffuse1d(phi, D, x, t):
 
     # always spherical
     # make sure t starts at 0
-    
+
     x_scld = x / x.max() # "R"
     phi_scld = phi / phi.max()
     t_scld = t / t.max()
 
     K = D / x.max()**2 * t.max()
-    
+
     dx = x_scld[1] - x_scld[0]
     dt = t_scld[1] - t_scld[0]
-    
+
     NX = x.size
     NT = t.size
 
@@ -55,13 +55,13 @@ def diffuse1d(phi, D, x, t):
 
     A1 = scipy.sparse.csr_matrix(A1)
     A2 = scipy.sparse.csr_matrix(A2)
-    
-    
+
+
     # u will store all of the time step solutions for now it just has
     # one element, the initial condition
 
     u = [phi_scld]
-    
+
 
     t1 = (K / dx**2) * A1
     t2 = K / dx * A2
@@ -74,8 +74,50 @@ def diffuse1d(phi, D, x, t):
         b = (I + dt * F).dot(u[-1])
         sol = sparse.linalg.spsolve(A, b)
         u.append(sol)
-        
-    return np.array(u) * phi.max()
-    
-    
 
+    return np.array(u) * phi.max()
+
+class Mixer(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __call__(self, atm):
+        pass
+
+class DiffusionMixer(Mixer):
+
+    def __init__(self, mixing_length, nt=2000):
+        if mixing_length <= 0:
+            raise ValueError("mixing length must be greater than 0.")
+        self.mixing_length = mixing_length
+        self.nt = nt
+
+    def __call__(self, atm):
+
+        # convert the mass fractions to concentrations
+        wei = np.asarray([ele.weight for ele in atm.spec])
+        phi = atm.rho_Msun_km3[:, None] / wei[None, :] * atm.comp
+
+        # recast the diffusion paramters somewhat
+        mixing_length_km = self.mixing_length * atm.texp
+        x_avg_km = atm.vgrid_avg * atm.texp
+        t = np.linspace(0, mixing_length_km**2, self.nt)
+
+        # do diffusion and store the results
+        newphiso = np.zeros((atm.nzones, atm.nspec))
+        newrhos = np.zeros((atm.nzones, atm.nspec))
+
+        for i in range(atm.nspec):
+            this_phi = phi.T[i]
+            new_phi = _diffuse1d(this_phi, 1., x_avg_km, t)[-1]
+            new_rho = wei[i] * new_phi
+            newphis[:, i] = new_phi
+            newrhos[:, i] = new_rho
+
+        # recompute atmospheric properties
+        rho_Msun_km3 = newrhos.sum(1)
+        comp = newrhos / rho_Msun_km3[:, None]
+
+        return MixedAtmosphere(atm.spec, comp, rho_Msun_km3,
+                               atm.nzones, atm.texp, atm.v_outer)
--- simple.py
+++ simple.py
@@ -1,207 +1,78 @@
+import abc
 from layers import *
 import numpy as np
 import diffuse1d
 import random
 
-__whatami__ = 'Simple supernova atmosphere generator.'
+__whatami__ = 'Simple supernova atmospheres.'
 __author__ = 'Danny Goldstein <dgold@berkeley.edu>'
 
-MSUN_G = 1.99e33
 KM_CM = 1e5
+MSUN_G = 1.99e33
 
-class SimpleAtmosphere(object):
-    """A simple supernova atmosphere. Composed of one or more Layers."""
-    
-    def __init__(self, layers, masses, mixing_length, specific_ke,
-                 nzones=100, v_outer=4.e4, texp=86400., nt=2000):
+class Atmosphere(object):
 
-        self.layers = layers
-
-        if len(set(self.layers)) != len(layers):
-            raise ValueError('Currently cannot handle atmospheres '\
-                                 'with repeated layers.')
-            
-        self.masses = masses
-        self.specific_ke = specific_ke
-        self.mixing_length = mixing_length
-
-        self.nzones = nzones
-        self.v_outer = v_outer
-        self.texp = texp
-        self.nt = nt
-
-        self.ejecta_mass = sum(self.masses)
-
-        # enumerate unique elements in the atmosphere
-        self.spec = [] 
-        for layer in self.layers:
-            self.spec += layer.abundances.keys()
-        self.spec = sorted(set(self.spec), key=lambda ele: ele.weight)[::-1]
-        
-        # initialize composition array
-        self.nspec = len(self.spec)
-        self.comp = np.zeros((self.nzones, self.nspec))
-        self.fracs = np.zeros_like(self.comp)
-
-        self.ke = self.specific_ke * self.ejecta_mass # erg 
-        self.ve = 2455 * (self.ke / 1e51)**0.5 # km / s
-        self.vgrid_outer = self.velocity(kind='outer') # km / s
-        self.vgrid_inner = self.velocity(kind='inner') # km / s
-        self.vgrid_avg   = self.velocity(kind='average') # km / s
-
-        # exponential density profile
-        self.interior_mass = 0.5 * (2.0 - np.exp(-self.vgrid_outer / self.ve) * \
-                                   (2.0 + (self.vgrid_outer / self.ve) * \
-                                        (2.0 + self.vgrid_outer / self.ve))) \
-                                        * self.ejecta_mass
-        
-        self.shell_mass = np.concatenate(([self.interior_mass[0]],
-                                          self.interior_mass[1:] - \
-                                          self.interior_mass[:-1]))
-
-        radii = [0]
-        for i in range(len(self.layers)):
-            radii.append(radii[i] + masses[i])
-        self.radii = np.asarray(radii[1:])
-        self.edge_shells = map(self.interior_mass.searchsorted, self.radii)
-
-        if self.v_outer < 8.4059 * self.ve:
-            raise ValueError('Model does not extend out far enough '
-                             'in velocity space to capture all the '
-                             'mass. Increase v_outer.')
-
-        # Traverse the zones in reverse order. 
-        for i in range(self.nzones)[::-1]:
-            sm = self.shell_mass[i]
-            isedge = np.asarray(self.edge_shells[:-1]) == i
-            nedge = isedge[isedge].size
-            if nedge == 0:
-                # Pure layer
-                bounds = [0] + self.edge_shells
-                for j in range(len(self.layers)):
-                    if bounds[j] <= i <= bounds[j+1]: # TODO: should one of these be strict?
-                        self.fracs[i, j] = 1.
-                        self.comp[i] = self._abun(self.fracs[i])
-
-            else:
-                # Transition layer
-
-                # ending is the index of the layer that exists in
-                # underlying zones that ends on entry into this zone.
-
-                # starting is the index of the layer that first
-                # appears in this zone.
-                
-                ending, kmax = np.argwhere(isedge)[[0, -1], 0]
-                starting = kmax + 1
-
-                nlayer = starting - ending + 1 # number of layers in this zone
-
-                # The mass in the overlying zones of this zone's
-                # topmost layer.
-                adv_mass = sum([self._layermass(self.layers[starting], l)
-                                for l in range(i+1, self.nzones)])
-
-                # The mass in this zone of this zone's topmost layer.
-                remaining_mass = self.masses[starting] - adv_mass
-
-                # The fraction in this zone of this zone's topmost layer.
-                self.fracs[i, starting] = remaining_mass / sm
-
-                # The fractions of the other layers:
-                for k in range(ending+1, starting)[::-1]:
-                    self.fracs[i, k] = self.masses[k] / sm
-                self.fracs[i, ending] = 1 - sum(self.fracs[i, ending+1:])
-                self.comp[i] = self._abun(self.fracs[i])
-            
-    #==============================================================================#
-    # Diffusion
-    #==============================================================================#
-
-        wei = np.asarray([ele.weight for ele in self.spec])
-        self.vol_cm3 = 4 * np.pi / 3 * (self.vgrid_outer**3 - \
-                                            self.vgrid_inner**3) * \
-                                            KM_CM**3 * self.texp**3
-
-        self.vol_km3 = self.vol_cm3 / KM_CM**3
-        self.rho_g_cm3 = self.shell_mass * MSUN_G / self.vol_cm3
-        self.rho_Msun_km3 = self.shell_mass / self.vol_km3
-        self.phi = self.rho_Msun_km3[:, None] / wei[None, :] * self.comp
-
-        self.mixing_length_km = self.mixing_length * self.texp
-        self.x_avg_km = self.vgrid_avg * self.texp
-
-        if self.mixing_length > 0:
-
-            newphis = []
-            newrhos = []
-            
-            self.D = 1. 
-            self.t = np.linspace(0,self.mixing_length_km**2,self.nt)
-
-            for i in range(len(wei)):
-
-                this_phi = self.phi.T[i]
-
-                new_phi = diffuse1d.diffuse1d(this_phi, self.D, self.x_avg_km, self.t)
-                new_phi = new_phi[-1]
-                new_rho = wei[i] * new_phi
-
-                newphis.append(new_phi)
-                newrhos.append(new_rho)
-
-            newphis = np.array(newphis).T
-            newrhos = np.array(newrhos).T
-
-            self.rho_Msun_km3 = newrhos.sum(1)
-            self.comp = newrhos / self.rho_Msun_km3[:, None]
-            
-            self.shell_mass = self.rho_Msun_km3 * self.vol_km3
-            self.rho_g_cm3 = self.shell_mass * MSUN_G / self.vol_cm3
-            self.interior_mass = np.cumsum(self.shell_mass)
-            
-        self.spec_mass = self.shell_mass[:, None] * self.comp
-
-    def _layermass(self, layer, i):
-        """The mass of layer `layer` in zone `i`."""
-        return self.shell_mass[i] * self.fracs[i, self.layers.index(layer)]
+    __metaclass__ = abc.ABCMeta
 
     def _indexof(self, element):
         """The index of element `element` in the species array `spec`."""
         return self.spec.index(element)
 
-    def _abun(self, fracs):
-        """Compute the composition array (mass fractions of each element, in
-        the order of self.spec), of an atmospheric zone composed of
-        `fracs` relative fractions of `layers` layers."""
+    @abc.abstractproperty
+    def ejecta_mass(self):
+        pass
 
-        result = np.zeros(self.nspec)
-        for layer, frac in zip(self.layers, fracs):
-            for elem in layer.abundances:
-                result[self._indexof(elem)] += layer.abundances[elem] * frac
-        return result
+    @abc.abstractproperty
+    def spec(self):
+        pass
+
+    @abc.abstractproperty
+    def nzones(self):
+        pass
+
+    @abc.abstractproperty
+    def comp(self):
+        pass
+
+    @abc.abstractproperty
+    def v_outer(self):
+        pass
+
+    @abc.abstractproperty
+    def rho_Msun_km3(self):
+        pass
+
+    @abc.abstractproperty
+    def interior_mass(self):
+        pass
+
+    @abc.abstractproperty
+    def texp(self):
+        pass
+
+    @property
+    def vol_cm3(self):
+        vo = self.velocity(kind='outer')
+        vi = self.velocity(kind='inner')
+        vol = 4 * np.pi / 3 * (vo**3 - vi**3) * KM_CM**3 * self.texp**3
+        return vol
+
+    @property
+    def vol_km3(self):
+        return self.vol_cm3 / KM_CM**3
+
+    @property
+    def shell_mass(self):
+        im = self.interior_mass
+        return np.concatenate(([im[0]], im[1:] - im[:-1]))
     
-    def velocity(self, kind='average'):
+    @property
+    def rho_g_cm3(self):
+        return self.rho_Msun_km3 * MSUN_G / (KM_CM)**3
 
-        """Zone radial velocities (km/s).
-
-        Parameters:
-        -----------
-        kind, str: 'inner', 'outer', and 'average'
-        """
-
-        if kind == 'outer':
-            return (self.v_outer / self.nzones) * \
-                np.arange(1, self.nzones + 1)
-        else:
-            boundary_vels = np.concatenate((np.asarray([0.]),
-                                               self.velocity(kind='outer')))
-            if kind == 'average':
-                return (boundary_vels[:-1] + boundary_vels[1:]) / 2.
-            elif kind == 'inner':
-                return boundary_vels[:-1]
-            else:
-                raise ValueError('kind must be one of inner, outer, average.')
+    @property
+    def spec_mass(self):
+        return self.shell_mass[:, None] * self.comp
 
     def plot(self, show=True):
         
@@ -226,9 +97,10 @@ class SimpleAtmosphere(object):
         colors = ['b','g','r','purple','c','m','y','k','orange','indigo','violet']
 
         ele = [elem.repr for elem in self.spec]
+        vga = self.velocity(kind='average')
                     
         for row,name,c in zip(self.comp.T,ele,colors):
-            axarr[0,0].semilogy(self.vgrid_avg,row,label=name,color=c)
+            axarr[0,0].semilogy(vga,row,label=name,color=c)
         axarr[0,0].set_ylabel('mass fraction')
         axarr[0,0].set_ylim(1e-3,1)
         axarr[0,0].legend(frameon=True,loc='best')
@@ -240,7 +112,7 @@ class SimpleAtmosphere(object):
         axarr[0,1].set_ylabel('mass fraction')
         axarr[0,1].set_ylim(1e-3,1)
         
-        axarr[1,0].semilogy(self.vgrid_avg,self.rho_Msun_km3)
+        axarr[1,0].semilogy(vga,self.rho_Msun_km3)
         axarr[1,0].set_ylabel('rho (Msun / km3)')
         axarr[1,0].set_xlabel('velocity (km/s)')
 
@@ -288,3 +160,211 @@ class SimpleAtmosphere(object):
         mod['time'] = np.asarray(self.texp).astype('<f8')
         mod['vol'] = self.vol_cm3
         mod.close()
+
+    def velocity(self, kind='average'):
+        """Zone radial velocities (km/s).
+
+        Parameters:
+        -----------
+
+        kind, str: 'inner', 'outer', and 'average'
+
+        """
+
+        if kind == 'outer':
+            return (self.v_outer / self.nzones) * \
+                np.arange(1, self.nzones + 1)
+        else:
+            boundary_vels = np.concatenate((np.asarray([0.]),
+                                               self.velocity(kind='outer')))
+            if kind == 'average':
+                return (boundary_vels[:-1] + boundary_vels[1:]) / 2.
+            elif kind == 'inner':
+                return boundary_vels[:-1]
+            else:
+                raise ValueError('kind must be one of inner, outer, average.')
+            
+
+class StratifiedAtmosphere(Atmosphere):
+    """A simple supernova atmosphere. Composed of one or more Layers."""
+
+    def __init__(self, layers, masses, profile, nzones=100, v_outer=4.e4,
+                 texp=86400.):
+
+        # state
+        self.layers = layers
+        self.profile = profile
+        self.masses = masses
+
+        # parameters
+        self._nzones = nzones
+        self._v_outer = v_outer
+        self._texp = texp
+
+        # grid
+        self.vgrid_outer = self.velocity(kind='outer') # km / s
+        self.vgrid_inner = self.velocity(kind='inner') # km / s
+        self.vgrid_avg   = self.velocity(kind='average') # km / s
+
+        # enumerate unique elements in the atmosphere
+        spec = [] 
+        for layer in self.layers:
+            spec += layer.abundances.keys()
+        self._spec = sorted(set(spec), key=lambda ele: ele.weight)[::-1]
+        
+        # initialize composition array
+        self.nspec = len(self.spec)
+        self._comp = np.zeros((self.nzones, self.nspec))
+        self.fracs = np.zeros_like(self._comp)
+
+        radii = [0]
+        for i in range(len(self.layers)):
+            radii.append(radii[i] + self.masses[i])
+        self.radii = np.asarray(radii[1:])
+        self.edge_shells = map(self.interior_mass.searchsorted, self.radii)
+
+        # if atmosphere does not contain >99% of the mass, raise an error
+        if not self.profile.accuracy_criterion(self.v_outer, .99):
+            raise ValueError('Model does not extend out far enough '
+                             'in velocity space to capture all the '
+                             'mass. Increase v_outer.')
+
+        # Traverse the zones in reverse order. 
+        for i in range(self.nzones)[::-1]:
+            sm = self.shell_mass[i]
+            isedge = np.asarray(self.edge_shells[:-1]) == i
+            nedge = isedge[isedge].size
+            if nedge == 0:
+                # Pure layer
+                bounds = [0] + self.edge_shells
+                for j in range(len(self.layers)):
+                    if bounds[j] <= i <= bounds[j+1]: # TODO: should one of these be strict?
+                        self.fracs[i, j] = 1.
+                        self._comp[i] = self._abun(self.fracs[i])
+
+            else:
+                # Transition layer
+
+                # ending is the index of the layer that exists in
+                # underlying zones that ends on entry into this zone.
+
+                # starting is the index of the layer that first
+                # appears in this zone.
+                
+                ending, kmax = np.argwhere(isedge)[[0, -1], 0]
+                starting = kmax + 1
+
+                nlayer = starting - ending + 1 # number of layers in this zone
+
+                # The mass in the overlying zones of this zone's
+                # topmost layer.
+                adv_mass = sum([self._layermass(self.layers[starting], l)
+                                for l in range(i+1, self.nzones)])
+
+                # The mass in this zone of this zone's topmost layer.
+                remaining_mass = self.masses[starting] - adv_mass
+
+                # The fraction in this zone of this zone's topmost layer.
+                self.fracs[i, starting] = remaining_mass / sm
+
+                # The fractions of the other layers:
+                for k in range(ending+1, starting)[::-1]:
+                    self.fracs[i, k] = self.masses[k] / sm
+                self.fracs[i, ending] = 1 - sum(self.fracs[i, ending+1:])
+                self._comp[i] = self._abun(self.fracs[i])
+                
+
+    @property
+    def spec(self):
+        return self._spec
+
+    @property
+    def ejecta_mass(self):
+        return sum(self.masses)
+
+    @property
+    def interior_mass(self):
+        vo = self.velocity(kind='outer')
+        return self.profile(vo)
+
+    @property
+    def comp(self):
+        return self._comp
+
+    @property
+    def nzones(self):
+        return self._nzones
+
+    @property
+    def v_outer(self):
+        return self._v_outer
+
+    @property
+    def texp(self):
+        return self._texp
+
+    @property
+    def rho_Msun_km3(self):
+        return self.shell_mass / self.vol_km3            
+
+    def _layermass(self, layer, i):
+        """The mass of layer `layer` in zone `i`."""
+        return self.shell_mass[i] * self.fracs[i, self.layers.index(layer)]
+
+    def _abun(self, fracs):
+        """Compute the composition array (mass fractions of each element, in
+        the order of self.spec), of an atmospheric zone composed of
+        `fracs` relative fractions of `layers` layers."""
+
+        result = np.zeros(self.nspec)
+        for layer, frac in zip(self.layers, fracs):
+            for elem in layer.abundances:
+                result[self._indexof(elem)] += layer.abundances[elem] * frac
+        return result
+    
+class MixedAtmosphere(Atmosphere):
+
+    @property
+    def nzones(self):
+        return self._nzones
+
+    @property
+    def v_outer(self):
+        return self._v_outer
+
+    @property
+    def texp(self):
+        return self._texp
+
+    @property
+    def spec(self):
+        return self._spec
+
+    @property
+    def rho_Msun_km3(self):
+        return self._rho_Msun_km3
+
+    @property
+    def ejecta_mass(self):
+        return self.interior_mass[-1]
+
+    @property
+    def comp(self):
+        return self._comp
+
+    @property
+    def interior_mass(self):
+        return np.cumsum(self.rho_Msun_km3 * self.vol_km3)
+
+    def __init__(self, spec, comp, dens, nzones, texp, v_outer):
+
+        # state
+        self._spec = spec
+        self._rho_Msun_km3 = dens
+        self._comp = comp
+        
+        # parameters
+        self._texp = texp
+        self._v_outer = v_outer
+        self._nzones = nzones
+        
