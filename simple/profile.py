add constants and fix bug in exponential KE calculation

simple/profile.py
@@ -1,5 +1,6 @@
 import abc
 import numpy as np
+from constants import *
 
 __whatami__ = 'Profiles (CDFs) for simple supernova atmospheres.'
 __author__ = 'Danny Goldstein <dgold@berkeley.edu>'
@@ -29,9 +30,10 @@ class Profile(object):
 class Exponential(Profile):
     """An exponential density profile."""
 
-    def __init__(self, ke):
+    def __init__(self, ke, mass):
         self.ke = ke  # erg
-        self.ve = 2455 * (self.ke / 1e51)**0.5  # km / s
+        self.mass = mass # msun
+        self.ve =  np.sqrt(ke / (mass * MSUN_G) / 6.) / KM_CM
 
     def __call__(self, v):
         return 0.5 * (2.0 - np.exp(-v / self.ve) *

simple/simple.py
@@ -1,6 +1,7 @@
 import abc
 from layers import *
 from profile import *
+from constants import *
 from scipy.integrate import quad
 import numpy as np
 import random
@@ -8,11 +9,6 @@ import random
 __whatami__ = 'Simple supernova atmospheres.'
 __author__ = 'Danny Goldstein <dgold@berkeley.edu>'
 __all__ = ['Atmosphere', 'StratifiedAtmosphere',  'MixedAtmosphere']
-
-KM_CM = 1e5
-MSUN_G = 1.99e33
-A = 7.5646e-15
-KB = 1.380648e-16  # erg / K
 
 class Atmosphere(object):
 